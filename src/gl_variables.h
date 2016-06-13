78481af278f7d6076a76910ce9134eb0455337e6
Leighton W. Wilson <leightonwilson@mac.com>
2016-06-13 10:11:51 -0700

    Changed var. declaration to "extern"

--- src/gl_variables.h
+++ src/gl_variables.h
@@ -1,6 +1,6 @@
 /*constant variables */
 
-double pi,one_over_4pi,bulk_coef,units_coef,epsw,epsp,eps,
+extern double pi,one_over_4pi,bulk_coef,units_coef,epsw,epsp,eps,
        bulk_strength,kappa2,kappa;
 
 /*#define pi 3.14159265358979324;
