9da4447282249381a19680a7ce713d94eaac7b06
Leighton W. Wilson <leightonwilson@mac.com>
2020-11-11 00:39:14 -0500

    updating wrap

--- src/tabipb_wrap/TABIPBWrap.h
+++ src/tabipb_wrap/TABIPBWrap.h
@@ -2,18 +2,13 @@
 #define H_TABIPB_WRAP_H
 
 #include "TABIPBStruct.h"
-
-#ifdef TABIPB_APBS
 #include "generic/valist.h"
-#endif
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-#ifdef TABIPB_APBS
 TABIPBOutput runTABIPBWrapAPBS(TABIPBInput tabipbIn, Valist* APBSMolecule);
-#endif
 
 #ifdef __cplusplus
 }
